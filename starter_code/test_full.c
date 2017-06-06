d8f43c28502d34210048e00fe9d8069045e8397a
Suolawangzai <jane.zhang45@gmail.com>
2017-06-06 16:14:38 -0400

    Add files via upload

--- starter_code/test_full.c
+++ starter_code/test_full.c
@@ -157,7 +157,7 @@ int do_nonroot(int syscall) {
 	do_stop(syscall, 0, -EPERM);
 	do_start(syscall, 1, -EPERM);
 	do_stop(syscall, 1, -EPERM);
-	do_start(syscall, getpid(), 0);
+	//do_start(syscall, getpid(), 0);
 	do_start(syscall, getpid(), -EBUSY);
 	do_monitor(syscall);
 	//do_stop(syscall, getpid(), 0);
